4c47072fb492f58e379f672c8068104314b153c8
ankulr <ankulsinghti1998@gmail.com>
2026-08-31 20:23:16 +0530

    Add project files.

---
+++ ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewsExample/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ViewsExample.Models
+{
+    public class Person
+    {
+        public string? PersonName { get; set; }
+
+        public DateTime?  DateOfBirth { get; set; }
+
+        public Gender PersonGender { get; set; }
+
+    }
+
+    public enum Gender
+    {
+        Male,Female,Trans
+    }
+}
